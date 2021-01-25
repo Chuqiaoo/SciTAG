9cae3bc0ac829b3216a7393e924e94b88e0525a2
cgong <cgong@ebi.ac.uk>
2021-01-25 16:46:05 +0000

    Initial Commit

---
+++ SciTAG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SciTAG
+//
+//  Created by Chuqiao Gong on 25/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SciTAG/SciTAGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SciTAGApp.swift
+//  SciTAG
+//
+//  Created by Chuqiao Gong on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SciTAGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
